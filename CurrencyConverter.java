1472e4dc153780d8e59fdb313e3dcc74c0beefed
Ravi Bhawsar <raviggbhawsar9090@gmail.com>
2024-12-01 13:01:40 +0530

    Add files via upload

---
+++ CurrencyConverter.java
@@ -0,0 +1,133 @@
+import java.util.Scanner;
+
+public class CurrencyConverter {
+    static Scanner sc = new Scanner(System.in);
+    static String convertingFrom, convertingTo;
+    static float initialCurrency, convertedCurrency, currencyFactor;
+
+    static void ConverterWelcome() {
+        System.out.println("!!Welcome to Currency Converter!!");// welcome!
+    }
+
+    static void CurrencyOptions() {
+        System.out.println("Write: " + "\"INR\" for Indian Rupees, " + "\"USD\" for US Dollar, " + "\"EUR\" for Euro and " + "\"AUD\" for Australian Dollar\n");//Options for Currencies!
+    }
+
+    static String getConvertingFrom() {
+        System.out.format("Converting From: ");// Converting form which currency?
+        return sc.nextLine();
+    }
+
+    static String getConvertingTo() {
+        System.out.format("Converting To: ");// Converting to which currency?
+        return sc.nextLine();
+    }
+
+    static float getInitialCurrency() {
+        System.out.format("Enter the Amount to be converted: ");
+        return sc.nextFloat();
+    }
+
+    static void ConvertingFrom_To_Matches() {
+
+        if (convertingFrom.equalsIgnoreCase("INR")) {
+
+            if (convertingTo.equalsIgnoreCase("USD")) {
+                currencyFactor = 0.012f;
+            } else if (convertingTo.equalsIgnoreCase("EUR")) {
+                currencyFactor = 0.011f;
+            } else if (convertingTo.equalsIgnoreCase("AUD")) {
+                currencyFactor = 0.018f;
+            } else {
+                System.out.println("Please choose a valid currency!");
+                CurrencyOptions();
+                convertingTo = getConvertingTo();
+                ConvertingFrom_To_Matches();
+            }
+        } else if (convertingFrom.equalsIgnoreCase("USD")) {
+
+            if (convertingTo.equalsIgnoreCase("INR")) {
+                currencyFactor = 84.56F;
+            } else if (convertingTo.equalsIgnoreCase("EUR")) {
+                currencyFactor = 0.95F;
+            } else if (convertingTo.equalsIgnoreCase("AUD")) {
+                currencyFactor = 1.54F;
+            } else {
+                System.out.println("Please choose a valid currency!");
+                CurrencyOptions();
+                convertingTo = getConvertingTo();
+                ConvertingFrom_To_Matches();
+            }
+        } else if (convertingFrom.equalsIgnoreCase("EUR")) {
+
+            if (convertingTo.equalsIgnoreCase("INR")) {
+                currencyFactor = 89.48F;
+            } else if (convertingTo.equalsIgnoreCase("USD")) {
+                currencyFactor = 1.06F;
+            } else if (convertingTo.equalsIgnoreCase("AUD")) {
+                currencyFactor = 1.63F;
+            } else {
+                System.out.println("Please choose a valid currency!");
+                CurrencyOptions();
+                convertingTo = getConvertingTo();
+                ConvertingFrom_To_Matches();
+            }
+        } else if (convertingFrom.equalsIgnoreCase("AUD")) {
+
+            if (convertingTo.equalsIgnoreCase("INR")) {
+                currencyFactor = 55.10F;
+            } else if (convertingTo.equalsIgnoreCase("EUR")) {
+                currencyFactor = 0.62F;
+            } else if (convertingTo.equalsIgnoreCase("USD")) {
+                currencyFactor = 0.65F;
+            } else {
+                System.out.println("Please choose a valid currency!");
+                CurrencyOptions();
+                convertingTo = getConvertingTo();
+                ConvertingFrom_To_Matches();
+            }
+        } else {
+            System.out.println("Please choose a valid currency!");
+            CurrencyOptions();
+            convertingFrom = getConvertingFrom();
+            convertingTo = getConvertingTo();
+            ConvertingFrom_To_Matches();
+        }
+    }
+
+    static void CurrencyConverterFunction() {
+        boolean convertAnotherCurrency = true;
+        char y_n;
+
+        ConverterWelcome();
+        while (convertAnotherCurrency) {
+            CurrencyOptions();
+
+            convertingFrom = getConvertingFrom();
+            convertingTo = getConvertingTo();
+
+            ConvertingFrom_To_Matches();
+
+            initialCurrency = getInitialCurrency();
+
+            convertedCurrency = initialCurrency * currencyFactor;
+            System.out.format("%s \"%.3f\" in %s is: \"%.3f\"\n", convertingFrom, initialCurrency, convertingTo, convertedCurrency);
+            System.out.println();
+
+            System.out.println("Do you want to convert more currencies?");
+            System.out.format("Write 'Y' for yes and 'N' for no: ");
+            y_n = sc.next().charAt(0);
+            sc.nextLine(); // Clear the newline character from the buffer
+            System.out.println();
+
+            if ((y_n != 'Y') && (y_n != 'y')) {
+                convertAnotherCurrency = false;
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        CurrencyConverterFunction();
+    }
+}
